1b8a1924e8592e031544c79bc425f41dcdce99fe
Vova-test <bezvitryy@gmail.com>
2021-04-07 20:09:38 +0300

    added languages

--- app/Viewers/Viewer.php
+++ app/Viewers/Viewer.php
@@ -4,8 +4,19 @@ namespace App\Viewers;
 
 trait Viewer
 {
+    protected $lang;
+
     public function view($template, $data = [])
     {
+        if (extract($data, EXTR_OVERWRITE)) {
+            unset($data);
+        }
+
         require TEMPLATE_FOLDER_PATH . "/" . $template . ".php";
+    }
+
+    public function setLanguage($lang = 'ua')
+    {
+        $this->lang = include(LANGUAGE_FOLDER_PATH . "/" . $lang . ".php");
     }
 }
