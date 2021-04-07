262fb130a5b0827aace26136e9291d47f431e6f5
Vova-test <bezvitryy@gmail.com>
2021-04-07 20:13:17 +0300

    added new routes

--- routes.php
+++ routes.php
@@ -3,5 +3,13 @@ return [
     "/" => [
         "controller" => "HomeController",
         "action" => "index"
-    ]
+    ],
+    "/today/index" => [
+        "controller" => "HomeController",
+        "action" => "indexToday"
+    ],
+    "/tenDays/index" => [
+        "controller" => "HomeController",
+        "action" => "indexTenDays"
+    ],
 ];
