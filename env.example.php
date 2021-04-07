49d07878ea180bb321faa6e1cfed240c1a59b230
Vova-test <bezvitryy@gmail.com>
2021-04-07 20:12:32 +0300

    changed env and added comments

--- env.example.php
+++ env.example.php
@@ -1,3 +1,8 @@
 <?php
-define("SITE_URL", "http://test.loc");
+//**** Rename this file like env.php
+//**** and change that SITE_URL for Yours!!!
+
+define("SITE_URL", "http://test2.loc");
 define("TEMPLATE", "templates");
+define("WEATHER_URL", "https://weather.com");
+define("LANGUAGE", "lang");
