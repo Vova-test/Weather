9ba044bf33a0be4a4bfd7b292404457721b0f54f
Vova-test <bezvitryy@gmail.com>
2021-04-13 21:58:03 +0300

    worked for delete errors

--- public/js/weather.js
+++ public/js/weather.js
@@ -1,53 +1,65 @@
 function search() {
+    let searchValue;
+    let searchLength;
+    let language;
     let length = 3;
-    let searchValue = " ";
-    let searchLength = " ";
-    let lenguage = $('#navbarDropdownMenuLink').attr('data-value');
     let locationType = "locale";
     let name = "getSunV3LocationSearchUrlConfig";
+    let delay = 500;
 
     $('#Search').on('input', function() {
         searchLength = $(this).val().length;
 
+        if (searchLength < length){
+            hideCities();
+        }
         if (searchLength >= length){
             searchValue = $(this).val();
+            language = $('#navbarDropdownMenuLink').attr('data-value');
 
-            $.ajax({
-                url: "https://weather.com/api/v1/p/redux-dal",
-                type: "POST",
-                dataType: "json",
-                data: JSON.stringify([{
-                    "name": name,
-                    "params":{
-                        "query":searchValue,
-                        "language":lenguage,
-                        "locationType": locationType
-                    }
-                }]),
-                contentType: "application/json",
-                success: function(data) {
-                    console.log("data", data);
-                    if (data.dal[`${name}`][`language:${lenguage};locationType:${locationType};query:${searchValue}`].status != '404'){
-                        let cityResponse = data.dal[`${name}`][`language:${lenguage};locationType:${locationType};query:${searchValue}`].data.location;
+            clearTimeout($(this).data('timer'));
+
+            $(this).data('timer', setTimeout(function(){
+                $(this).removeData('timer');
+
+                $.ajax({
+                    url: "https://weather.com/api/v1/p/redux-dal",
+                    type: "POST",
+                    dataType: "json",
+                    data: JSON.stringify([{
+                        "name": name,
+                        "params":{
+                            "query":searchValue,
+                            "language":language,
+                            "locationType": locationType
+                        }
+                    }]),
+                    contentType: "application/json",
+                    success: function(data) {
+                        if (typeof(data.dal[`${name}`][`language:${language};locationType:${locationType};query:${searchValue}`]) == "undefined") {
+                            alert("Not Defind!!!");
+                            return;
+                        }
+                        if (data.dal[`${name}`][`language:${language};locationType:${locationType};query:${searchValue}`].status != '200'){
+                            alert(data.dal[`${name}`][`language:${language};locationType:${locationType};query:${searchValue}`].statusText);
+                            return;
+                        }
+
+                        let cityResponse = data.dal[`${name}`][`language:${language};locationType:${locationType};query:${searchValue}`].data.location;
 
                         setCityList(cityResponse);
+                    },
+                    error: function(xhr, ajaxOptions, thrownError) {
+                        alert(thrownError + "\r\n" + xhr.statusText + "\r\n" + xhr.responseText);
                     }
-                    /*console.log("dal", data.dal[`${name}`][`language:${lenguage};locationType:${locationType};query:${searchValue}`].status);
-                    let cityResponse = data.dal[`${name}`][`language:${lenguage};locationType:${locationType};query:${searchValue}`].data.location;
-
-                    setCityList(cityResponse);*/
-                },
-                error: function(data) {
-                    let errorData = data;
-                }
-            });
+                });
+            }, delay));
         }
     });
 
     $('#Search').on('focusout', function() {
         $(this).val('');
-        $('#cityCase').hide();
-        $('#cityCase').children().slice(1).remove();
+        hideCities();
     });
 }
 
@@ -66,10 +78,23 @@ function setCityList(cityData) {
     $('#cityCase').show();
 }
 
+function clickCity() {
+    //$('#cityCase').
+}
+
 function setLenguage() {
     $('#langCase').children('.dropdown-item').on('click', function() {
-        $('#navbarDropdownMenuLink').text( $(this).text());
+        $(this).siblings('*[disabled]').removeAttr('disabled');
+        $(this).attr('disabled', true);
+
+        $('#navbarDropdownMenuLink').text($(this).text());
+        $('#navbarDropdownMenuLink').attr('data-value', $(this).attr('data-value'));
     });
+}
+
+function hideCities(){
+    $('#cityCase').hide();
+    $('#cityCase').children().slice(1).remove();
 }
 
 $(document).ready(function(){
